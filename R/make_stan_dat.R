953ade028c9eecbba6755deafb8daef474b5c21b
andrewdolman <andydolman@gmail.com>
2017-04-05 10:51:11 +0200

    setup sections to start a little above the first depth, and end below the last depth. Buffer is 5%, but negative depths not allowed.

--- R/make_stan_dat.R
+++ R/make_stan_dat.R
@@ -7,29 +7,41 @@
 #'
 #' @examples
 make_stan_dat <- function(depth, obs_age, obs_err, K = 10, nu = 6,
-                       acc_mean, acc_var = "default",
-                       mem_mean = 0.7, mem_strength = 4) {
+                          acc_mean, acc_var = "default",
+                          mem_mean = 0.7, mem_strength = 4, ...) {
   
   # Pretty sure nu = 6 is equivalent to the default parameterisation of t.a = 3, t.b = 4 in Bacon
   # when t.b = t.a +1, there are 2a degrees of freedom, and the distribution is symetric.
   
-  l <- list(depth=depth, obs_age=obs_age, obs_err=obs_err)
-  l$N = length(depth)
-  l$K = K
-  l$nu = nu
-  l$acc_mean = acc_mean
+  l <- c(as.list(environment()), list(...))
+  
+  # Transformed arguments
+  l$N <- length(l$depth)
+  
   if (acc_var == "default") {
     l$acc_var = acc_mean^2 / 1.5}else{
       l$acc_var = acc_var
     }
-  # l$mem_alpha = mem_strength * mem_mean
-  # l$mem_beta = mem_strength * (1-mem_mean)
+  l$mem_alpha = mem_strength * mem_mean
+  l$mem_beta = mem_strength * (1-mem_mean)
+  
   l$mem_mean = mem_mean
   l$mem_strength = mem_strength
   l$c = 1:K
-  l$delta_c = diff(range(depth)) / K
-  l$c_depth_bottom = l$delta_c * l$c + depth[1]
-  l$c_depth_top = c(0, l$c_depth_bottom[1:(K-1)])
+  
+  
+  # Set start depth to 5% less than first depth observation, but do not allow negative depths
+  depth_range <- diff(range(depth))
+  buff_5 <- 0.05 * depth_range
+  strt_dpth <- depth[1] - buff_5
+  strt_dpth[strt_dpth < 0] <- 0
+  end_dpth <- tail(depth, 1) + buff_5
+  depth_range = end_dpth - strt_dpth
+  l$delta_c = depth_range / K
+  l$c_depth_bottom = l$delta_c * l$c + strt_dpth
+  l$c_depth_top = c(strt_dpth, l$c_depth_bottom[1:(K-1)])
+  
+  # Index for which sections the target depth is in
   l$which_c = sapply(l$depth, function(d) which.max((l$c_depth_bottom < d) * (l$c_depth_bottom - d) ))
   return(l)
 } 
