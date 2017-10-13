f2c87b62bf5ce71081ffe7446fb034b8d29b2b42
andrewdolman <andydolman@gmail.com>
2017-10-13 22:04:40 +0200

    add prior and posterior plots to plot_stan_bacon.R

--- R/plot_stan_bacon.R
+++ R/plot_stan_bacon.R
@@ -10,6 +10,8 @@
 #'
 #' @return A ggplot2 object
 #' @export
+#' @importFrom ggpubr ggarrange
+#' @importFrom rstan extract
 #' @examples
 plot_stan_bacon <- function(stan_bacon_fit, n.iter = 1000) {
   
@@ -33,7 +35,7 @@ plot_stan_bacon <- function(stan_bacon_fit, n.iter = 1000) {
     tidyr::gather(Section, Age,-Iter) %>%
     dplyr::left_join(., c_depths, by = "Section")
   
-  p <- post_depth_age %>%
+  p.fit <- post_depth_age %>%
     dplyr::filter(Iter %in% sample(unique(.$Iter), n.iter, replace = FALSE)) %>%
     ggplot2::ggplot(aes(x = Depth, y = Age, group = Iter)) +
     ggplot2::geom_line(alpha = 0.5 / sqrt(n.iter))  +
@@ -45,6 +47,42 @@ plot_stan_bacon <- function(stan_bacon_fit, n.iter = 1000) {
       alpha = 0.5) +
     ggplot2::theme_bw()
   
-  p
+  
+  ## Prior and posterior figures
+  acc.rng <- qgamma(c(0.000001, 0.999), shape = stan_bacon_fit$data$acc_alpha,  rate = stan_bacon_fit$data$acc_beta)
+  
+  acc.prior <- tibble(acc.rate = seq(acc.rng[1], acc.rng[2], length.out = 1000)) %>% 
+    mutate(acc.dens = dgamma(acc.rate, shape = stan_bacon_fit$data$acc_alpha,  rate = stan_bacon_fit$data$acc_beta))
+  
+  acc.post <- tibble(alpha = as.vector(rstan::extract(stan_bacon_fit$fit, "alpha")$alpha))
+  
+  p.acc <- acc.prior %>% 
+    ggplot(aes(x = acc.rate, y = acc.dens)) +
+    geom_density(data = acc.post, aes(x = alpha), fill = "Grey", inherit.aes = FALSE) +
+    geom_line(colour = "Red") + 
+    scale_x_continuous("Acc. rate [yr/cm]", limits = acc.rng) +
+    scale_y_continuous("Density") +
+    theme_bw()
+  
+  # memory prior
+  mem.prior <- tibble(mem = seq(0, 1, length.out = 1000)) %>% 
+    mutate(mem.dens = dbeta(mem, shape1 = stan_bacon_fit$data$mem_alpha,  shape2 = stan_bacon_fit$data$mem_beta))
+  
+  mem.post <- tibble(R = as.vector(rstan::extract(stan_bacon_fit$fit, "R")$R),
+                     w = as.vector(rstan::extract(stan_bacon_fit$fit, "w")$w))
+  
+  p.mem <- mem.prior %>% 
+    ggplot(aes(x = mem, y = mem.dens)) +
+    geom_density(data = mem.post, aes(x = R), fill = "Grey", inherit.aes = FALSE) +
+    #geom_density(data = mem.post, aes(x = w), fill = "Grey", inherit.aes = FALSE) +
+    geom_line(colour = "Red") + 
+    scale_x_continuous("Memory [correlation at 1 cm]", limits = c(0, 1)) + 
+    scale_y_continuous("") +
+    theme_bw()
+  
+  ggpubr::ggarrange(
+    ggpubr::ggarrange(p.acc, p.mem, ncol = 2),
+    p.fit,
+    nrow = 2, heights = c(1, 2)) 
 }
 
