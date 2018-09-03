55fecef585690a234ea0292ef8b738e4f94df0e0
Andrew Dolman <andydolman@gmail.com>
2018-09-03 16:36:30 +0200

    restructure package to work with new version of rstan_package_skeleton and fix compilation issues

--- R/baconr-package.R
+++ R/baconr-package.R
@@ -4,5 +4,11 @@
 #' @name baconr-package
 #' @aliases baconr
 #' @useDynLib baconr, .registration = TRUE 
-#' @exportPattern("^[[:alpha:]]+")
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
 NULL
