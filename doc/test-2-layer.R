45b99fdb401c73769ca90212a078522538763dfe
Andrew Dolman <andydolman@gmail.com>
2020-06-23 16:51:56 +0200

    tidy Stan code

--- doc/test-2-layer.R
+++ doc/test-2-layer.R
@@ -4,38 +4,53 @@ library(tidyverse)
 
 
 ## New make stan dat
+insertLayer <- function(P, after=0, ...) {
+  #  P     : Plot object
+  # after  : Position where to insert new layers, relative to existing layers
+  #  ...   : additional layers, separated by commas (,) instead of plus sign (+)
+
+  if (after < 0)
+    after <- after + length(P$layers)
+
+  if (!length(P$layers))
+    P$layers <- list(...)
+  else
+    P$layers <- append(P$layers, list(...), after)
+
+  return(P)
+}
 
 make_stan_dat_2 <- function(depth, obs_age, obs_err,
                           hiatus_depth = NULL, hiatus_length = NULL,
                           hiatus_shape = 1, hiatus_interval = 0.1,
                           K1 = 10, K = 100, nu = 6,
                           record_prior_acc_mean_mean = 20, record_prior_acc_mean_shape = 1.5,
-                          record_prior_acc_shape_mean = 1.5, 
+                          record_prior_acc_shape_mean = 1.5,
                           record_prior_acc_shape_shape = 1.5,
-                          
+
                           mem_mean = 0.7, mem_strength = 4, ...) {
-  
+
   # Pretty sure nu = 6 is equivalent to the default parameterisation of t.a = 3, t.b = 4 in Bacon
   # when t.b = t.a +1, there are 2a degrees of freedom, and the distribution is symetric.
-  
+
   l <- c(as.list(environment()), list(...))
-  
+
   # Transformed arguments
   l$N <- length(depth)
-  
+
   stopifnot(K%%K1 == 0)
   l$whichK1 = rep(1:K1, each = K / K1)
-  
+
   #l$acc_alpha = rep(acc_alpha, K1)
   #l$acc_beta =  acc_alpha / acc_mean
-  
-  
+
+
   l$mem_alpha = mem_strength * mem_mean
   l$mem_beta = mem_strength * (1-mem_mean)
-  
+
   l$mem_mean = mem_mean
   l$mem_strength = mem_strength
-  
+
   # Set start depth to 5% less than first depth observation, but do not allow negative depths
   depth_range <- diff(range(depth))
   buff_5 <- 0.05 * depth_range
@@ -43,11 +58,11 @@ make_stan_dat_2 <- function(depth, obs_age, obs_err,
   strt_dpth[strt_dpth < 0] <- 0
   end_dpth <- tail(depth, 1) + buff_5
   depth_range = end_dpth - strt_dpth
-  
+
   # Hiatus data
-  
+
   if (is.null(hiatus_depth) == FALSE){
-    
+
     stopifnot(length(hiatus_depth) == length(hiatus_length))
     if (any(hiatus_depth < min(depth))) {
       stop("One or more hiatus is shallower than the minimum core depth")
@@ -55,14 +70,14 @@ make_stan_dat_2 <- function(depth, obs_age, obs_err,
     if (any(hiatus_depth > max(depth))) {
       stop("One or more hiatus is deeper than the maximum core depth")
     }
-    
+
     l$hiatus_depth = as.array(hiatus_depth)
     l$hiatus_length = as.array(hiatus_length)
-    
+
     l$N_hiatus = length(hiatus_depth)
     l$hiatus_shape = hiatus_shape
     l$hiatus_beta = as.array(hiatus_shape / hiatus_length)
-    
+
     # Generate as equal as possible sections accounting for hiatuses
     # delta_c needs to be a vector because not all sections will be equally long
     pos <- sort(c(strt_dpth, hiatus_depth, hiatus_depth+hiatus_interval, end_dpth))
@@ -73,44 +88,45 @@ make_stan_dat_2 <- function(depth, obs_age, obs_err,
     n_dpths <- length(dpths)
     l$c_depth_top = dpths[1:(n_dpths-1)]
     l$c_depth_bottom = dpths[2:n_dpths]
-    
+
     # number of sections may not always be exactly the K requested
     l$K = length(l$c_depth_bottom)
-    
+
     l$delta_c = diff(dpths)
-    
+
     # Index for which sections the target depth is in
     l$which_c = sapply(l$depth, function(d) which.max((l$c_depth_bottom < d) * (l$c_depth_bottom - d) ))
-    
+
     # Index for which sections the hiatuses are in
     l$which_c_hiatus = as.array(sapply(l$hiatus_depth, function(d) which.max((l$c_depth_top < d) * (l$c_depth_top - d))))
-    
+
     # used for setting w = 0 at hiatus transitions
     l$hiatus_factor = rep(1, l$K)
     l$hiatus_factor[l$which_c_hiatus] <- 0
-    
+
   }else{
     l$c = 1:K
     l$delta_c = depth_range / K
     l$c_depth_bottom = l$delta_c * l$c + strt_dpth
     l$c_depth_top = c(strt_dpth, l$c_depth_bottom[1:(K-1)])
-    
+
     # Index for which sections the target depth is in
     l$which_c = sapply(l$depth, function(d) which.max((l$c_depth_bottom < d) * (l$c_depth_bottom - d) ))
   }
-  
+
   return(l)
-} 
+}
 
 
-name <- "TIANCHIYL"
-dat <- read.csv(paste0("../envi-age-modelling/working-data/terr_14C_min10_dates-2020.03.04_15-19-42/", name, "/", name,".csv")) %>% 
+name <- "BEBR5"
+dat <- read.csv(paste0("/Users/andrewdolman/Dropbox/Work/AWI/Data/terrestrial-age-models/terr_14C_min10_dates-2020.03.04_15-19-42/", name, "/", name,".csv")) %>%
+#dat <- read.csv(paste0("../envi-age-modelling/working-data/terr_14C_min10_dates-2020.03.04_15-19-42/", name, "/", name,".csv")) %>%
   filter(depth > 0)
 
-dat <- ecustools::CalibrateAge(dat, age.14C = "age", age.14C.se = "error") %>% 
+dat <- ecustools::CalibrateAge(dat, age.14C = "age", age.14C.se = "error") %>%
   filter(complete.cases(age.14C.cal))
 
-dat %>% 
+dat %>%
   ggplot(aes(x = depth, y = age.14C.cal)) +
   geom_point() +
   geom_line()
@@ -125,15 +141,16 @@ options(mc.cores = parallel::detectCores())
 #options(mc.cores = 1)
 
 dat.fit.2l <- make_stan_dat_2(
-  depth = dat$depth, 
-  obs_age = dat$age.14C.cal, 
+  inflate_errors = 0,
+  depth = dat$depth,
+  obs_age = dat$age.14C.cal,
   obs_err = dat$age.14C.cal.se,
   K = 100,
   K1 = 10,
   nu = 6,
   record_prior_acc_mean_mean = acc.mean,
   record_prior_acc_mean_shape = 1.5,
-  record_prior_acc_shape_mean = 1.5, 
+  record_prior_acc_shape_mean = 1.5,
   record_prior_acc_shape_shape = 1.5,
   mem_mean = 0.7, mem_strength = 4,
   iter = 1000, chains = 3)
@@ -145,11 +162,11 @@ GetInits <- function(dat){
     alpha = abs(rnorm(dat$K, dat$record_prior_acc_mean_mean, dat$record_prior_acc_mean_mean/3)),
     record_acc_mean = abs(rnorm(1, dat$record_prior_acc_mean_mean, dat$record_prior_acc_mean_mean/3)),
     section_acc_mean = abs(rnorm(dat$K1, dat$record_prior_acc_mean_mean, dat$record_prior_acc_mean_mean/3)),
-    
+
     record_acc_shape = rnorm(1, 1.5, 1.5/3),
-   
+
     age0 = runif(1, 0, dat$obs_age),
-    
+
     infl_mean = abs(rnorm(1, 0, 0.1)),
     infl_sd = abs(rnorm(1, 0, 0.1)),
     infl = abs(rnorm(dat$N, 0, 0.1))
@@ -165,136 +182,87 @@ twoFit.fit <- rstan::sampling(two_l_mod, data = dat.fit.2l, chains = 3, init = i
 
 twoFit <- list(fit = twoFit.fit, data = dat.fit.2l)
 
-s1 <- summary(twoFit$fit) 
-s1 <- as_tibble(s1$summary, rownames = "par") 
+s1 <- summary(twoFit$fit)
+s1 <- as_tibble(s1$summary, rownames = "par")
+
+samples <- as.data.frame(twoFit.fit) %>% as_tibble()
+
 
 range(s1$Rhat)
-hist(s1$n_eff, 100)
+range(s1$n_eff)
 
-
+options(width = 85)
 print(twoFit$fit, par = c("record_acc_mean"))
-
-print(twoFit$fit, par = c("infl"))
-print(twoFit$fit, par = c("obs_err_1"))
-
-print(twoFit$fit, par = c("infl_mean"))
-
-print(twoFit$fit, par = c("infl_shape"))
-print(twoFit$fit, par = c("infl_alpha"))
-print(twoFit$fit, par = c("infl_beta"))
-
-
-alpha <- s1 %>% 
-  filter(par == "infl_alpha") %>% pull(mean)
-
-beta <- s1 %>% 
-  filter(par == "infl_beta") %>% pull(mean)
-
-
-x <- seq(-0.1, 10, length.out = 1000)
-d <- dgamma(x, shape = alpha, rate = beta)
-plot(x, d, type = "l", xlim = c(0, 11))
-
-
-print(twoFit$fit, par = c("record_acc_shape"))
 
 print(twoFit$fit, par = c("section_acc_mean"))
 
+print(twoFit$fit, par = c("infl_shape", "infl_beta", "infl_mean"))
+print(twoFit$fit, par = c("infl"))
+
 print(twoFit$fit, par = c("w", "R"))
 
-
-s1 %>% 
-  filter(grepl("section_acc_mean", par)) %>% 
-  filter(grepl("beta", par) == FALSE) %>% 
-  pull(mean) %>% hist(., 20)
-
-s1 %>% 
-  filter(grepl("alpha", par)) %>% 
-  pull(mean) %>% hist(., 20)
-
-
-s1 %>% 
-  filter(grepl("infl[", par, fixed = T)) %>% 
-  pull(mean) %>% hist(., 20)
-
-
-insertLayer <- function(P, after=0, ...) {
-  #  P     : Plot object
-  # after  : Position where to insert new layers, relative to existing layers
-  #  ...   : additional layers, separated by commas (,) instead of plus sign (+)
-  
-  if (after < 0)
-    after <- after + length(P$layers)
-  
-  if (!length(P$layers))
-    P$layers <- list(...)
-  else 
-    P$layers <- append(P$layers, list(...), after)
-  
-  return(P)
-}
-
 set.seed(20170406)
-p.multilevel <- plot_stan_bacon(twoFit, 1000, plot_priors = F) 
+p.multilevel <- plot_stan_bacon(twoFit, 1000, plot_priors = F)
 #p.multilevel
 
 # infl errors
-infl <- s1 %>% 
+infl <- s1 %>%
   filter(grepl("infl[", par, fixed = T))
 
 dat.infl <- tibble(depth = twoFit$data$depth, obs_age = twoFit$data$obs_age,
                    obs_err = twoFit$data$obs_err,
-                   infl = infl$mean) %>% 
+                   infl = infl$mean) %>%
   mutate(infl.err = obs_err + obs_err * infl)
 
 insertLayer(p.multilevel, after = 1,
             geom_linerange(data = dat.infl,
-                           aes(x = depth, 
+                           aes(x = depth,
                                y = obs_age,
                                ymax = obs_age + 2*infl.err,
                                ymin = obs_age - 2*infl.err),
                            colour = "green", group = NA)) +
   labs(subtitle = paste0("Inflation factor = ",
                          paste0(round(infl$mean, 1), collapse = ", ")))
-  
+
 
 
 #ggsave(paste0("doc/", name, "multilevel.png"), p.multilevel)
 
-options(width = 85)
-print(twoFit$fit, par = c("record_acc_mean"))
-print(twoFit$fit, par = c("infl"))
-print(twoFit$fit, par = c("infl_mean"))
-
-
-
 rstan::traceplot(twoFit$fit, par = "infl", inc_warmup = T)
+
 rstan::traceplot(twoFit$fit, par = "section_acc_mean", inc_warmup = F)
 
 rstan::traceplot(twoFit$fit, par = "record_acc_shape", inc_warmup = T)
 
-d2 <- s1 %>% 
-  filter(grepl("alpha", par)) %>% 
-  filter(grepl("beta", par) == FALSE) %>% 
-  mutate(depth = 0.5+(1:200)/20) 
+d2 <- s1 %>%
+  filter(grepl("alpha", par)) %>%
+  filter(grepl("beta", par) == FALSE) %>%
+  mutate(depth = 0.5+(1:100)/10)
 
-s1 %>% 
-  filter(grepl("section_acc_mean", par)) %>% 
-  filter(grepl("beta", par) == FALSE) %>% 
-  mutate(depth = 1:10) %>% 
+s1 %>%
+  filter(grepl("section_acc_mean", par)) %>%
+  filter(grepl("beta", par) == FALSE) %>%
+  mutate(depth = 1:10) %>%
   ggplot(aes(x = depth, y = mean)) +
   geom_point() +
   geom_line() +
   geom_line(data = d2, colour = "Red")
 
 
+# mean infl is not neccesarily the same as mean(shape) / mean(beta)
+samples %>%
+  select(starts_with("infl_")) %>%
+  mutate(mean = infl_shape / infl_beta) %>%
+  summarise_if(is.numeric, mean) %>%
+  mutate(mean2 = infl_shape / infl_beta)
+
 
 
 #######
 
 dat.fit <- stan_bacon_hier(
-  depth = dat$depth, 
-  obs_age = dat$age.14C.cal, 
+  depth = dat$depth,
+  obs_age = dat$age.14C.cal,
   obs_err = dat$age.14C.cal.se,
   K = 100,
   nu = 6,
--- inst/stan/bacon_2_layer_hier_acc_mean.stan
+++ inst/stan/bacon_2_layer_hier_acc_mean.stan
@@ -3,18 +3,18 @@
 // Add addtional layer to alphas.
 data {
   int<lower=0> N;
-  int<lower=0> K;
-  int<lower=0> K1;
-  int<lower=0> nu;
+  int<lower=0> K;  // no of fine sections
+  int<lower=0> K1; // no of coarse sections
+  int<lower=0> nu; // degrees of freedom of t error distribution
   vector[N] depth;
   vector[N] obs_age;
   vector[N] obs_err;
   vector[K] c_depth_bottom;
   vector[K] c_depth_top;
-  int whichK1[K];
-  int which_c[N];
+  int whichK1[K]; // index fine sections to their parent coarse sections
+  int which_c[N]; // index observations to their fine section
   real<lower = 0> delta_c;
-  
+
   // hyperparameters
   real<lower = 0> record_prior_acc_mean_mean;
   real<lower = 0> record_prior_acc_mean_shape;
@@ -23,39 +23,33 @@ data {
   real<lower = 0> record_prior_acc_shape_mean;
   real<lower = 0> record_prior_acc_shape_shape;
 
-  //vector<lower=0>[K1] acc_alpha;
-  //real<lower=0> acc_alpha;
-  
   real<lower = 0> mem_mean;
   real<lower = 0> mem_strength;
 }
 transformed data{
   // transformed hyperparameters
-  real<lower=0> record_prior_acc_mean_alpha;
   real<lower=0> record_prior_acc_mean_beta;
 
-  real<lower=0> record_prior_acc_shape_alpha;
   real<lower=0> record_prior_acc_shape_beta;
-  
-  
+
+
   real<lower=0> mem_alpha;
   real<lower=0> mem_beta;
-  
+
   // depths corresponding to c_ages
   //  vector[K+1] c_depths;
-  
-  record_prior_acc_mean_alpha = record_prior_acc_mean_shape;
+
+
   record_prior_acc_mean_beta = record_prior_acc_mean_shape / record_prior_acc_mean_mean;
 
-  record_prior_acc_shape_alpha = record_prior_acc_shape_shape;
   record_prior_acc_shape_beta = record_prior_acc_shape_shape / record_prior_acc_shape_mean;
- 
- 
+
+
   mem_alpha = mem_strength * mem_mean;
   mem_beta = mem_strength * (1-mem_mean);
-  
+
   //  c_depths = append_row(c_depth_top[1], c_depth_bottom);
-  
+
 }
 parameters {
   real<lower = 0, upper = 1> R;
@@ -64,67 +58,76 @@ parameters {
   real<lower = 0> record_acc_mean;
   real<lower = 0> record_acc_shape;
   vector<lower=0>[K1] section_acc_mean;
-  
+
   // measurement error inflation factor
   real<lower = 0> infl_mean;
   real<lower = 0> infl_shape;
   vector<lower = 0>[N] infl;
+
 }
 transformed parameters{
-  
-  //real<lower=0> acc_alpha;
-  //vector<lower=0>[K1] acc_beta;
+
   real<lower = 0> record_acc_mean_beta;
-  //real<lower = 0> record_acc_shape_beta;
-  
+
   real<lower = 0, upper = 1> w;
-  
+
   vector[K] x;
   vector[K+1] c_ages;
   vector[N] Mod_age;
   vector[K1] section_acc_mean_beta;
-  
-  real infl_beta;
-  real infl_alpha = infl_shape;
-  
+
+   real infl_beta;
+
+  // the inflated observation errors
   vector[N] obs_err_1 = obs_err + infl .* obs_err;
-  
+
   infl_beta = infl_shape / infl_mean;
-  
+
   record_acc_mean_beta = record_acc_shape / record_acc_mean;
 
   //record_acc_shape_beta = record_prior_acc_shape_shape / record_prior_acc_shape_mean;
-  
-  section_acc_mean_beta = record_prior_acc_mean_alpha ./ section_acc_mean;
-  
+
+  section_acc_mean_beta = record_prior_acc_mean_shape ./ section_acc_mean;
+
   w = R^(delta_c);
-  
+
   x[1] = alpha[1];
-  
-  // call to sampling function is already vectorised below so there is no 
+
+  // call to sampling function is already vectorised below so there is no
   // advantage to vectorising here
   // x[2:K] = w * x[1:(K-1)] + (1-w) * alpha[2:K];
-  
+
   for(i in 2:K){
     x[i] = w*x[i-1] + (1-w)*alpha[i];
   }
-  
+
   // Get the Mod_ages
   c_ages[1] = age0;
   c_ages[2:(K+1)] = age0 + cumulative_sum(x * delta_c);
   Mod_age = c_ages[which_c] + x[which_c] .* (depth - c_depth_top[which_c]);
 }
 model {
+
+  // parameters for the hierarchical prior on the section means
+  record_acc_mean ~ gamma(record_prior_acc_mean_shape, record_prior_acc_mean_beta);
+  record_acc_shape ~ gamma(record_prior_acc_shape_shape, record_prior_acc_shape_beta);
+
+  // the K1 section means, parametrised by the record mean and a user specified shape (alpha)
+  section_acc_mean ~ gamma(record_prior_acc_mean_shape, record_acc_mean_beta);
+
+  // the Gamma distributed innovations
+  // parametrised from the section mean acc rates and a user supplied shape (alpha)
+  alpha ~ gamma(record_prior_acc_mean_shape, section_acc_mean_beta[whichK1]);
+
+  // loosely model the first age as being anywhere between 0 and the youngest data point
+  age0 ~ uniform(0, obs_age[1]);
+
+  R ~ beta(mem_alpha, mem_beta);
+
+  // the observation model
   infl_mean ~ gamma(1.5, 1);
   infl_shape ~ gamma(1.5, 1);
-  infl ~ gamma(infl_alpha, infl_beta);
-  record_acc_mean ~ gamma(record_prior_acc_mean_alpha, record_prior_acc_mean_beta);
-  record_acc_shape ~ gamma(record_prior_acc_shape_alpha, record_prior_acc_shape_beta);
-  section_acc_mean ~ gamma(record_prior_acc_mean_alpha, record_acc_mean_beta);
-  //acc_var ~ gamma(acc_var_alpha, acc_var_beta);
-  age0 ~ uniform(0, obs_age[1]);
-  R ~ beta(mem_alpha, mem_beta);
-  alpha ~ gamma(record_prior_acc_mean_alpha, section_acc_mean_beta[whichK1]);
-  //obs_age ~ normal(Mod_age, obs_err);
+  infl ~ gamma(infl_shape, infl_beta);
+
   obs_age ~ student_t(nu, Mod_age, obs_err_1);
 }
