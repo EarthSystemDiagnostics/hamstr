bfaf7bb0afb65907d2d28b4c22b08961bea175b5
Andrew Dolman <andydolman@gmail.com>
2021-10-19 17:15:31 +0200

    package building code

---
+++ package-dev-scripts/package-building.R
@@ -0,0 +1,15 @@
+# devtools::document()
+# pkgbuild::compile_dll(force = TRUE)
+# devtools::load_all()
+
+# devtools::install(quick = FALSE)
+# devtools::install(quick = FALSE, dependencies = FALSE)
+# devtools::install(quick = FALSE, build = FALSE, dependencies = FALSE)
+# devtools::install(quick = TRUE, build = TRUE, dependencies = FALSE)
+
+
+# using ../rstanlm because already inside the rstanlm directory
+install.packages("../hamstr", repos = NULL, type = "source", INSTALL_opts = "--no-multiarch", clean = TRUE)
+
+
+remotes::install_github("earthsystemdiagnostics/hamstr", args = "--preclean", build_vignettes = FALSE)
